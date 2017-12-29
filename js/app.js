29ceac540b2002da90a865340c92a806013ccf4b
zeku87 <nocomment182@gmail.com>
2017-12-29 17:46:11 +0100

    API call to get weather conditions: T and icon

--- js/app.js
+++ js/app.js
@@ -23,4 +23,13 @@ $(".main-clock").text(time);
 //Set date from date.js today function
 $(".main-date").text(today());
 
-
+//Let's call weather API to get the icon code and current temperature
+const appid = "ac40fa4914a844cbb43379bcbab81383"; 
+const url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + appid;
+var iconId = "";
+var currentTemp = "";
+$.getJSON(url, (jsonFile)=>{
+	iconId = jsonFile.weather[0].icon;
+	currentTemp = jsonFile.main.temp;
+	currentTemp -= 273.16; //Kelvin to ºC 
+});
--- www/js/app.js
+++ www/js/app.js
@@ -23,4 +23,14 @@ $(".main-clock").text(time);
 //Set date from date.js today function
 $(".main-date").text(today());
 
-
+//Let's call weather API to get the icon code and current temperature
+const appid = "ac40fa4914a844cbb43379bcbab81383"; 
+const url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + appid;
+var iconId = "";
+var currentTemp = "";
+$.getJSON(url, (jsonFile)=>{
+	iconId = jsonFile.weather[0].icon;
+	currentTemp = jsonFile.main.temp;
+console.log(iconId);
+console.log(currentTemp - 273.16);
+});
