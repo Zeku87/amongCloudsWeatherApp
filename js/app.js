e595139fe13f8c5da2356276c96e7b312a12accc
zeku87 <nocomment182@gmail.com>
2017-12-29 17:57:14 +0100

    Render T and icon

--- js/app.js
+++ js/app.js
@@ -25,11 +25,17 @@ $(".main-date").text(today());
 
 //Let's call weather API to get the icon code and current temperature
 const appid = "ac40fa4914a844cbb43379bcbab81383"; 
-const url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + appid;
+const url = "http://api.openweathermap.org/data/2.5/weather?q=cadiz,es&appid=" + appid;
 var iconId = "";
 var currentTemp = "";
 $.getJSON(url, (jsonFile)=>{
 	iconId = jsonFile.weather[0].icon;
 	currentTemp = jsonFile.main.temp;
-	currentTemp -= 273.16; //Kelvin to ºC 
+	currentTemp -= 273.16; //Kelvin to ºC
+	currentTemp = currentTemp.toFixed(2);
+
+	//Show information
+	$(".weather-location-weathersymbol").attr("src", "http://openweathermap.org/img/w/" + iconId + ".png");
+	$(".weather-temperature").text(currentTemp);
+
 });
